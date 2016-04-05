63b372ded543613f4cc8d0984f98e43427345fb6
dmccloskey <dmccloskey87@gmail.com>
2016-04-04 19:06:18 -0700

    added new _js method

--- SBaaS_resequencing/stage01_resequencing_gd_io.py
+++ SBaaS_resequencing/stage01_resequencing_gd_io.py
@@ -150,7 +150,6 @@ class stage01_resequencing_gd_io(stage01_resequencing_gd_query,
         '''export data_stage01_resequencing_mutationsAnnotated to js file
         Visualization: scatterlineplot of mutation frequency across jump time
         '''
-        
         genomediff = genome_diff();
         print('exportingdataStage01ResequencingMutationsAnnotated...')
         # get the analysis information
@@ -303,7 +302,12 @@ class stage01_resequencing_gd_io(stage01_resequencing_gd_query,
             return data_json_O;
         with open(filename_str,'w') as file:
             file.write(ddtutilities.get_allObjects());        
-    def export_dataStage01ResequencingMutationsSeqChanges_js(self,analysis_id_I,mutation_id_exclusion_list=[],frequency_threshold=0.1,data_dir_I="tmp"):
+    def export_dataStage01ResequencingMutationsSeqChanges_js(self,
+                analysis_id_I,
+                mutation_id_exclusion_list=[],
+                frequency_threshold=0.1,
+                max_position=4630000,
+                data_dir_I="tmp"):
         '''export data_stage01_resequencing_mutationsSeqChanges to js file'''
         
         genomediff = genome_diff();
@@ -311,128 +315,62 @@ class stage01_resequencing_gd_io(stage01_resequencing_gd_query,
         # get the analysis information
         experiment_ids,sample_names,time_points = [],[],[];
         experiment_ids,sample_names,time_points = self.get_experimentIDAndSampleNameAndTimePoint_analysisID_dataStage01ResequencingAnalysis(analysis_id_I);
-        ## convert time_point to intermediates
-        #time_points_int = [int(x) for x in time_points];
-        #intermediates,time_points,experiment_ids,sample_names = (list(t) for t in zip(*sorted(zip(time_points_int,time_points,experiment_ids,sample_names))))
-        #intermediates = [i for i,x in enumerate(intermediates)];
-        mutation_data_O = [];
-        mutation_ids = [];
+        data_O = [];
         for sample_name_cnt,sample_name in enumerate(sample_names):
             # query mutation data:
             mutations = [];
             mutations = self.get_mutations_experimentIDAndSampleName_dataStage01ResequencingMutationsSeqChanges(experiment_ids[sample_name_cnt],sample_name);
             for end_cnt,mutation in enumerate(mutations):
-                if mutation['mutation_position'] > 4000000: #ignore positions great than 4000000
+                if mutation['mutation_position'] > max_position:
                     continue;
                 if mutation['mutation_frequency']<frequency_threshold:
                     continue;
                 # mutation id
                 mutation_id = genomediff._make_mutationID2(mutation['mutation_genes'],mutation['mutation_type'],mutation['mutation_position'],mutation['dna_sequence_ref'],mutation['dna_sequence_new']);
-                #mutation_id = self._make_mutationID2(mutation['mutation_genes'],mutation['mutation_type'],mutation['mutation_position']);
+                if mutation_id in mutation_id_exclusion_list:
+                    continue;
                 tmp = {};
                 tmp.update(mutation);
                 tmp.update({'mutation_id':mutation_id});
-                mutation_data_O.append(tmp);
-                mutation_ids.append(mutation_id);
-        # screen out mutations
-        mutation_ids_screened = [x for x in mutation_ids if x not in mutation_id_exclusion_list];
-        mutation_ids_unique = list(set(mutation_ids_screened));
-        # get mutation information for all unique mutations
-        mutation_ids_uniqueInfo = [];
-        for mutation_id in mutation_ids_unique:
-            for mutation in mutation_data_O:
-                if mutation_id == mutation['mutation_id']:
-                    tmp = {};
-                    
-                    tmp['experiment_id']=mutation['experiment_id'];
-                    tmp['sample_name']=mutation['sample_name'];
-                    tmp['mutation_id']=mutation['mutation_id'];
-                    tmp['mutation_type']=mutation['mutation_type'];
-
-                    tmp['mutation_frequency']=mutation['mutation_frequency'];
-                    #if mutation['mutation_annotations']:
-                    #    tmp['mutation_annotations']=";".join([x for x in mutation['mutation_annotations'] if x is not None]);
-                    #else: tmp['mutation_annotations']=mutation['mutation_annotations'];
-                    #if mutation['mutation_links']:
-                    #    tmp['mutation_links']=";".join([x for x in mutation['mutation_links'] if x is not None]);
-                    #else: tmp['mutation_links']=mutation['mutation_links'];
-
-                    if mutation['mutation_genes']:
-                        tmp['mutation_genes']=";".join([x for x in mutation['mutation_genes'] if x is not None]);
-                    else: tmp['mutation_genes']=mutation['mutation_genes'];
-                    if mutation['mutation_position']:
-                        tmp['mutation_position']=mutation['mutation_position'];
-                    else: tmp['mutation_position']=mutation['mutation_position'];
-                    if mutation['mutation_locations']:
-                        tmp['mutation_locations']=";".join([x for x in mutation['mutation_locations'] if x is not None]);
-                    else: tmp['mutation_locations']=mutation['mutation_locations'];
-                    
-                    tmp['dna_sequence_ref']=mutation['dna_sequence_ref'];
-                    tmp['dna_sequence_new']=mutation['dna_sequence_new'];
-                    tmp['rna_sequence_ref']=mutation['rna_sequence_ref'];
-                    tmp['rna_sequence_new']=mutation['rna_sequence_new'];
-                    tmp['peptide_sequence_ref']=mutation['peptide_sequence_ref'];
-                    tmp['peptide_sequence_new']=mutation['peptide_sequence_new'];
-
-                    if mutation['mutation_class']:
-                        tmp['mutation_class']=";".join([x for x in mutation['mutation_class'] if x is not None]);
-                    else: tmp['mutation_class']=mutation['mutation_class'];
-                    if mutation['dna_features_region']:
-                        tmp['dna_features_region']=";".join([x for x in mutation['dna_features_region'] if x is not None]);
-                    else: tmp['dna_features_region']=mutation['dna_features_region'];
-                    if mutation['rna_features_region']:
-                        tmp['rna_features_region']=";".join([x for x in mutation['rna_features_region'] if x is not None]);
-                    else: tmp['rna_features_region']=mutation['rna_features_region'];
-                    if mutation['peptide_features_region']:
-                        tmp['peptide_features_region']=";".join([x for x in mutation['peptide_features_region'] if x is not None]);
-                    else: tmp['peptide_features_region']=mutation['peptide_features_region'];
-
-                    tmp['dna_feature_position']=mutation['dna_feature_position'];
-                    tmp['dna_feature_ref']=mutation['dna_feature_ref'];
-                    tmp['dna_feature_new']=mutation['dna_feature_new'];
-                    tmp['rna_feature_position']=mutation['rna_feature_position'];
-                    tmp['rna_feature_ref']=mutation['rna_feature_ref'];
-                    tmp['rna_feature_new']=mutation['rna_feature_new'];
-                    tmp['peptide_feature_position']=mutation['peptide_feature_position'];
-                    tmp['peptide_feature_ref']=mutation['peptide_feature_ref'];
-                    tmp['peptide_feature_new']=mutation['peptide_feature_new'];
-                    
-                    tmp['mutation_data']=json.dumps(mutation['mutation_data']);
-                    tmp['used_']=mutation['used_'];
-                    tmp['comment_']=mutation['comment_'];
-                    mutation_ids_uniqueInfo.append(tmp);
-                    #break;
-        data_O = mutation_ids_uniqueInfo;
-        #data_O = [];
-        ## add in 0.0 frequency for mutations that are not found
-        #for sample_name_cnt,sample_name in enumerate(sample_names):
-        #    for mutation_id in mutation_ids_uniqueInfo:
-        #        tmp = {};
-        #        tmp_fitted = {};
-        #        tmp['mutation_id']=mutation_id['mutation_id']
-        #        tmp['time_point']=time_points[sample_name_cnt]
-        #        #tmp['intermediate']=intermediates[sample_name_cnt]
-        #        tmp['experiment_id']=experiment_ids[sample_name_cnt]
-        #        tmp['sample_name']=sample_name
-        #        tmp['mutation_frequency']=0.0;  
-        #        tmp['mutation_genes']=mutation_id['mutation_genes'];
-        #        tmp['mutation_position']=mutation_id['mutation_position'];
-        #        tmp['mutation_links']=mutation_id['mutation_links'];
-        #        tmp['mutation_type']=mutation_id['mutation_type'];
-        #        tmp['mutation_data']=mutation_id['mutation_data'];
-        #        tmp['used_']=mutation_id['used_'];
-        #        tmp['comment_']=mutation_id['comment_'];
-        #        for mutation in mutation_data_O:
-        #            if sample_name == mutation['sample_name'] and mutation_id['mutation_id'] == mutation['mutation_id']:
-        #                tmp['mutation_frequency']=mutation['mutation_frequency'];
-        #                tmp['comment_']=mutation['comment_'];
-        #                data_O.append(tmp);
-        #                break;
+                # format data
+                if mutation['mutation_genes']:
+                    tmp['mutation_genes']=";".join([x for x in mutation['mutation_genes'] if x is not None]);
+                else: tmp['mutation_genes']=mutation['mutation_genes'];
+                if mutation['mutation_position']:
+                    tmp['mutation_position']=mutation['mutation_position'];
+                else: tmp['mutation_position']=mutation['mutation_position'];
+                if mutation['mutation_locations']:
+                    tmp['mutation_locations']=";".join([x for x in mutation['mutation_locations'] if x is not None]);
+                else: tmp['mutation_locations']=mutation['mutation_locations'];
+                if mutation['mutation_class']:
+                    tmp['mutation_class']=";".join([x for x in mutation['mutation_class'] if x is not None]);
+                else: tmp['mutation_class']=mutation['mutation_class'];
+                if mutation['dna_features_region']:
+                    tmp['dna_features_region']=";".join([x for x in mutation['dna_features_region'] if x is not None]);
+                else: tmp['dna_features_region']=mutation['dna_features_region'];
+                if mutation['rna_features_region']:
+                    tmp['rna_features_region']=";".join([x for x in mutation['rna_features_region'] if x is not None]);
+                else: tmp['rna_features_region']=mutation['rna_features_region'];
+                if mutation['peptide_features_region']:
+                    tmp['peptide_features_region']=";".join([x for x in mutation['peptide_features_region'] if x is not None]);
+                else: tmp['peptide_features_region']=mutation['peptide_features_region'];
+                #generate an html reprentation of the sequence change
+                dna_ref_html,dna_new_html,rna_ref_html,rna_new_html,peptide_ref_html,peptide_new_html = genomediff._make_mutationSeqChangeHTML(mutation);
+                tmp['dna_sequence_ref']=dna_ref_html;
+                tmp['dna_sequence_new']=dna_new_html;
+                tmp['rna_sequence_ref']=rna_ref_html;
+                tmp['rna_sequence_new']=rna_new_html;
+                tmp['peptide_sequence_ref']=peptide_ref_html;
+                tmp['peptide_sequence_new']=peptide_new_html;
+                #peptide length
+                tmp['peptide_length_ref']=len(mutation['peptide_sequence_ref']);
+                tmp['peptide_length_new']=len(mutation['peptide_sequence_new']);
+                data_O.append(tmp);
         # dump chart parameters to a js files
         data1_keys = [
                     'experiment_id',
                     'sample_name',
-                    'time_point',
+                    #'time_point',
                     'mutation_id',
                     'mutation_type',
                     'mutation_position',
@@ -455,6 +393,11 @@ class stage01_resequencing_gd_io(stage01_resequencing_gd_query,
             'tileclass':"panel panel-default",'rowclass':"row",'colclass':"col-sm-12"};
         formparameters_O = {'htmlid':'filtermenuform1','htmltype':'form_01',"formsubmitbuttonidtext":{'id':'submit1','text':'submit'},"formresetbuttonidtext":{'id':'reset1','text':'reset'},"formupdatebuttonidtext":{'id':'update1','text':'update'}};
         formtileparameters_O.update(formparameters_O);
+        # make the DNA html object
+        # make the RNA html object
+        # make the Peptide html object
+
+        # make the table parameter object
         tableparameters_O = {"tabletype":'responsivetable_01',
                     'tableid':'table1',
                     "tablefilters":None,
@@ -466,26 +409,44 @@ class stage01_resequencing_gd_io(stage01_resequencing_gd_query,
                                     'mutation_genes',
                                     'mutation_locations',
                                     'mutation_class',
-                                    'dna_feature_position',
-                                    'dna_feature_ref',
-                                    'dna_feature_new',
-                                    'rna_feature_position',
-                                    'rna_feature_ref',
-                                    'rna_feature_new',
-                                    'peptide_feature_position',
-                                    'peptide_feature_ref',
-                                    'peptide_feature_new',
                                     'dna_sequence_ref',
                                     'dna_sequence_new',
                                     'rna_sequence_ref',
                                     'rna_sequence_new',
                                     'peptide_sequence_ref',
                                     'peptide_sequence_new',
-                                    'dna_features_region',
-                                    'rna_features_region',
-                                    'peptide_features_region',
+                                    'peptide_feature_position',
+                                    'peptide_length_ref',
+                                    'peptide_length_new',
                                     'used_',
                                     'comment_'],
+                    #"tableheaders":['experiment_id',
+                    #                'sample_name',
+                    #                'mutation_type',
+                    #                'mutation_position',
+                    #                'mutation_genes',
+                    #                'mutation_locations',
+                    #                'mutation_class',
+                    #                'dna_feature_position',
+                    #                'dna_feature_ref',
+                    #                'dna_feature_new',
+                    #                'rna_feature_position',
+                    #                'rna_feature_ref',
+                    #                'rna_feature_new',
+                    #                'peptide_feature_position',
+                    #                'peptide_feature_ref',
+                    #                'peptide_feature_new',
+                    #                'dna_sequence_ref',
+                    #                'dna_sequence_new',
+                    #                'rna_sequence_ref',
+                    #                'rna_sequence_new',
+                    #                'peptide_sequence_ref',
+                    #                'peptide_sequence_new',
+                    #                'dna_features_region',
+                    #                'rna_features_region',
+                    #                'peptide_features_region',
+                    #                'used_',
+                    #                'comment_'],
                     "tableclass":"table  table-condensed table-hover",
     			    'tableformtileid':'filtermenu1','tableresetbuttonid':'reset1','tablesubmitbuttonid':'submit1'};
         tabletileparameters_O = {'tileheader':'Mutation sequence analysis','tiletype':'table','tileid':"tile2",'rowid':"row2",'colid':"col1",
@@ -502,7 +463,7 @@ class stage01_resequencing_gd_io(stage01_resequencing_gd_query,
             return data_json_O;
         with open(filename_str,'w') as file:
             file.write(ddtutilities.get_allObjects());     
-    def export_dataStage01ResequencingMutationsSeqChangesAndAnnotated_js(self,analysis_id_I,mutation_id_exclusion_list=[],frequency_threshold=0.1,data_dir_I="tmp"):
+    def export_dataStage01ResequencingMutationsSeqChangesAndAnnotated_js(self,analysis_id_I,mutation_id_exclusion_list=[],frequency_threshold=0.1,max_position=4630000,data_dir_I="tmp"):
         '''export join of data_stage01_resequencing_mutationsSeqChanges and data_stage01_resequencing_mutationsAnnotated to js file'''
         
         genomediff = genome_diff();
@@ -522,7 +483,7 @@ class stage01_resequencing_gd_io(stage01_resequencing_gd_query,
             mutations = self.get_mutations_experimentIDAndSampleName_dataStage01ResequencingMutationsSeqChangesAndAnnotated(experiment_ids[sample_name_cnt],sample_name);
             if not mutations: continue;
             for end_cnt,mutation in enumerate(mutations):
-                if mutation['mutation_position'] > 4000000: #ignore positions great than 4000000
+                if mutation['mutation_position'] > max_position: #ignore positions great than 4000000
                     continue;
                 if mutation['mutation_frequency']<frequency_threshold:
                     continue;

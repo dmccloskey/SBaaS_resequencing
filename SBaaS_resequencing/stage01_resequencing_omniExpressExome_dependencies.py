25cf1858ab150fb3d3fbeb9fd1f7a09451381fb2
Douglas McCloskey <dmccloskey87@gmail.com>
2017-03-21 21:13:15 +0100

    update to omniExpressExome

---
+++ SBaaS_resequencing/stage01_resequencing_omniExpressExome_dependencies.py
@@ -0,0 +1,15 @@
+class stage01_resequencing_omniExpressExome_dependencies():
+    def make_mutationID(self,Chr,MapInfo):
+    """Return unique mutation id based on the
+    chromosome and position
+
+    Args
+        Chr (str)
+        MapInfo (int)
+    
+    Returns
+        id_O (str)
+    """
+
+    id_O = '%_%'%(Chr,MapInfo)
+    return id_O;

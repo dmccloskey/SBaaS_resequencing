fc7107b57d1030580ee22ff2eea02ee4486eaf77
dmccloskey <dmccloskey87@gmail.com>
2017-03-21 15:37:33 -0700

    fixed indentation error

--- SBaaS_resequencing/stage01_resequencing_omniExpressExome_dependencies.py
+++ SBaaS_resequencing/stage01_resequencing_omniExpressExome_dependencies.py
@@ -1,15 +1,15 @@
 class stage01_resequencing_omniExpressExome_dependencies():
     def make_mutationID(self,Chr,MapInfo):
-    """Return unique mutation id based on the
-    chromosome and position
+        """Return unique mutation id based on the
+        chromosome and position
 
-    Args
-        Chr (str)
-        MapInfo (int)
+        Args
+            Chr (str)
+            MapInfo (int)
     
-    Returns
-        id_O (str)
-    """
+        Returns
+            id_O (str)
+        """
 
-    id_O = '%_%'%(Chr,MapInfo)
-    return id_O;
+        id_O = '%s_%s'%(Chr,MapInfo)
+        return id_O;
